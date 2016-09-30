88230403a371ee99d9126377d096cd223105baf2
Roberto Fernandez Cueto <roberfern@gmail.com>
2016-09-30 10:49:51 +0200

    Development of the wrapper.

--- svnwrapper.sh
+++ svnwrapper.sh
@@ -2,5 +2,51 @@
 # This script is just a wrapper that holds the username and password on the 
 # local variables and allowing the user to navigate inside repositories without
 # having to reauthenticate and allowing the user to use svn commands easily.
-# $Format:Author:%an<%ae> - Commiter:%cn <%ce> - %cd$
+# $Format:Author:%an <%ae> - Commiter:%cn <%ce> - %cd$
+
+SERVER_URL=${1}
+
+if [ -z "${SERVER_URL}" ] ; then
+    read -p "Enter the SVN Server's URL: " SERVER_URL
+fi
+
+USERNAME=
+PASSWORD=
+SVN=$(which svn || which svnlite)
+SVN_COMMANDS=$(${SVN} help | awk ' BEGIN { filter = 1; } /^Available subcommands:$/ { filter = 0; next; } { if (filter == 1) next; else if ($0 ~ /^[[:space:]]/ ) print $0; }'| tr '[(),]' ' ')
+
+read -p "Username: " USERNAME
+
+stty_origin=$(stty -g)
+stty -echo
+read -p "Password: " PASSWORD
+stty ${stty_origin}
+unset stty_origin
+echo
+
+while true ; do
+    read -p "svn> " CMD URL OPTIONS
+    [ "${URL}" = "." ] && URL=""
+
+    if [ "${CMD}" = "exit" -o "${CMD}" = "quit" ] ; then
+        exit 0
+    fi
+
+    do_continue=true
+    for ITEM in ${SVN_COMMANDS} ; do
+        if [ "${ITEM}" = "${CMD}" ] ; then
+            do_continue=false
+            break
+        fi
+    done
+
+    if ${do_continue} ; then
+        echo "--- Unkown command ${CMD} ---"
+        continue
+    fi
+
+    ${SVN} --no-auth-cache --username "${USERNAME}" --password ${PASSWORD} ${CMD} \
+       ${SERVER_URL}/${URL} ${OPTIONS}
+    [ ${?} -eq 0 -a -n "${URL}" ] && SERVER_URL="${SERVER_URL}/${URL}"
+done
 
