8f42acf256795999eba7924841230b08c1e0ad37
Roberto Fernandez Cueto <roberfern@gmail.com>
2016-09-30 10:06:23 +0200

    Adding the initial header.

---
+++ svnwrapper.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# This script is just a wrapper that holds the username and password on the 
+# local variables and allowing the user to navigate inside repositories without
+# having to reauthenticate and allowing the user to use svn commands easily.
+# $Format:Author:%an<%ae> - Commiter:%cn <%ce> - %cd$
+
